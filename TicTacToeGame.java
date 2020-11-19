160e4e225cb908e196e1f6c185c836d5b0ea4230
samiksha Shende <shende.samiksha@rediffmail.com>
2020-11-19 11:42:44 +0530

    Added : UC2- Asking for input from user

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,19 +1,41 @@
 package tictactoe;
 
-public class TicTacToeGame {
-	
-	// UC1 - creating method to initialize indices with blank space
-		static char[] createBoard() {
-			char[] board = new char[10];
-			for (int index = 1; index < board.length; index++) {
-				board[index] = ' ';
-			}
-			return board;
-		}
-	
-		public static void main(String[] args) {
+import java.util.Scanner;
 
-			System.out.println("Welcome to TicTacToe Game");
-			createBoard();
-}
+public class TicTacToeGame {
+	// UC1 - creating method to initialize indices with blank space
+	static char[] createBoard() {
+		char[] board = new char[10];
+		for (int index = 1; index < board.length; index++) {
+			board[index] = ' ';
+		}
+		return board;
+	}
+
+	// UC2 - Creating method to take the input from user
+	public static void userInput() {
+
+		char computer = 'O';
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter the input X/O");
+
+		char userInput = input.next().toUpperCase().charAt(0);
+
+		if (userInput == 'X') {
+			computer = 'O';
+		} else if (userInput == 'O') {
+			computer = 'X';
+		} else {
+			System.out.println("Invalid input");
+		}
+		input.close();
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to TicTacToe Game");
+		createBoard();
+		userInput();
+	}
 }
