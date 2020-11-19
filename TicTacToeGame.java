bf31ace17fe3130205dea678c9e6fd0cebc1d550
samiksha Shende <shende.samiksha@rediffmail.com>
2020-11-19 10:34:46 +0530

    Created : UC1 - Initializing elements of array with blank space

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -2,10 +2,18 @@ package tictactoe;
 
 public class TicTacToeGame {
 	
+	// UC1 - creating method to initialize indices with blank space
+		static char[] createBoard() {
+			char[] board = new char[10];
+			for (int index = 1; index < board.length; index++) {
+				board[index] = ' ';
+			}
+			return board;
+		}
 	
 		public static void main(String[] args) {
 
 			System.out.println("Welcome to TicTacToe Game");
-
+			createBoard();
 }
 }
